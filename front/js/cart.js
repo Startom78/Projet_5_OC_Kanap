4c95101eee37beefe973e9cf5f8795e9bcf37ee3
Startom78 <tom78630@gmail.com>
2023-12-13 15:16:00 +0100

    displayed things on website

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,71 +1,132 @@
 // Je crée une fonction pour récupérer mon panier dans le local storage et afficher mon panier
 let cart = JSON.parse(localStorage.getItem('cart')) || [];
-const API_URL = "http://localhost:3000/api/products"
-console.log(API_URL)
 console.log(cart)
-function displayCart () {
-    
-    if (cart.length <= 0) {
+function displayCart() {
 
-      window.alert("Votre panier est vide !")
-    }
-    
-    fetch(API_URL)
-    .then(response => response.json())      
-    let cartItemsElt= document.getElementById('cart_items')
-    cart.forEach((product) => {
-        
-        let productOfCartElt = document.createElement('article') 
-        productOfCartElt.classList.add("cart__item")
-        productOfCartElt.dataset.id = product.id
-        productOfCartElt.dataset.color = product.color 
+  if (cart.length <= 0) {
 
-        let productImgDiv = document.createElement('div') 
-        productImgDiv.classList.add('cart__item__img')
+    window.alert("Votre panier est vide !")
+  }
 
-        let productImg = document.createElement('img')
-        productImg.src = ` ${product.imageUrl} `
-        productImg.alt = "Photographie d'un canapé"
-        productImgDiv.appendChild(productImg)
-        productOfCartElt.appendChild(productImgDiv)
+  cart.forEach(async (product) => {
 
-        let productDiv = document.createElement('div')
-        productDiv.classList.add('cart__item__content')
-        
-        let productDivInfo = document.createElement('div')
-        productDivInfo.classList.add('cart__item__content__description')
+    const apiInfos = await getProducts(product.id) // Je récupère mes infos sur l'API ici
 
-        let productDivInfoH2 = document.createElement('h2')
-        productDivInfoH2.textContent = product.name
-        productDivInfo.appendChild(productDivInfoH2)
-         
-        productDiv.appendChild(productDivInfo)
-        productOfCartElt.appendChild(productDiv)
-   
-        console.log(productOfCartElt) 
-        console.log(product.imageUrl)
-    }) 
-    /* <!--  <article class="cart__item" data-id="{product-ID}" data-color="{product-color}"> 
-            <div class="cart__item__img">
-              <img src="../images/product01.jpg" alt="Photographie d'un canapé">
-            </div>
-            <div class="cart__item__content">     
-              <div class="cart__item__content__description">
-                <h2>Nom du produit</h2>
-                <p>Vert</p>
-                <p>42,00 €</p>
-              </div>
-              <div class="cart__item__content__settings">
-                <div class="cart__item__content__settings__quantity">
-                  <p>Qté : </p>
-                  <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
-                </div>
-                <div class="cart__item__content__settings__delete">
-                  <p class="deleteItem">Supprimer</p>
-                </div>
-              </div>
-            </div> */
+    let display = ` <article class="cart__item" data-id="${apiInfos._id}" data-color="${product.color}">   
+      <div class="cart__item__img">
+        <img src="${apiInfos.imageUrl}" alt="${apiInfos.altTxt}">
+      </div>
+      <div class="cart__item__content">
+        <div class="cart__item__content__description">
+          <h2>${apiInfos.name}</h2>
+          <p>${product.color}</p>
+          <p>${apiInfos.price} €</p>
+      </div> 
+      <div class="cart__item__content__settings">
+        <div class="cart__item__content__settings__quantity">
+          <p>Qté : ${product.quantity} </p>
+          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${product.quantity}>
+        </div>
+        <div class="cart__item__content__settings__delete">
+          <p class="deleteItem">Supprimer</p>
+        </div>
+      </div>`
+            
+    document.querySelector('#cart__items').insertAdjacentHTML('beforeend', display) // J'injecte mon code js dans le HTML et je le mets en avant
+  })
+}
+displayCart()
+
+function totalPrice () { // Je crée une fonction qui va calculer et afficher le prix total de mon panier
+
+  
+
+}
+
+function modifyQuantity() { // Je crée une fonction qui va me permettre d'ajouter ou d'enlever des éléments de mon panier
+
+
+
+
+
+
+
 
 
 }
-displayCart()
+
+
+
+
+
+
+
+
+
+
+
+/*let productOfCartElt = document.getElementById('cart__items')
+console.log(productOfCartElt)
+productOfCartElt.dataset.id = product.id
+productOfCartElt.dataset.color = product.color
+
+let productImgDiv = document.createElement('div')
+productImgDiv.classList.add('cart__item__img')
+
+let productImg = document.createElement('img')
+productImg.src = apiInfos.imageUrl
+productImg.alt = apiInfos.altTxt
+productImgDiv.appendChild(productImg)
+productOfCartElt.appendChild(productImgDiv)
+
+let productDiv = document.createElement('div')
+productDiv.classList.add('cart__item__content')
+
+let productDivInfo = document.createElement('div')
+productDivInfo.classList.add('cart__item__content__description')
+
+let productDivInfoH2 = document.createElement('h2')
+h2 = apiInfos.name
+console.log(h2)
+productDivInfo.appendChild(productDivInfoH2)
+productDiv.appendChild(productDivInfo)
+productOfCartElt.appendChild(productDiv)
+
+let productDivInfoColor = document.createElement('p')
+const color = product.color
+productDivInfo.appendChild(productDivInfoColor)
+console.log(color)
+
+let productDivInfoPrice = document.createElement('p')
+price = +apiInfos.price
+productDivInfo.appendChild(productDivInfoPrice)
+console.log(price) 
+
+//console.log(document.getElementsByClassName('cart__item'))
+//console.log(document.getElementsByClassName('cart__item').insertAdjacentHTML)
+//cartItemsElt.appendChild(product)
+//document.getElementsByClassName('cart__item').insertAdjacentHTML("beforeend", cart)
+
+})
+
+}
+/* <!--  <article class="cart__item" data-id="{product-ID}" data-color="{product-color}"> 
+    <div class="cart__item__img">
+      <img src="../images/product01.jpg" alt="Photographie d'un canapé">
+    </div>
+    <div class="cart__item__content">     
+      <div class="cart__item__content__description">
+        <h2>Nom du produit</h2>
+        <p>Vert</p>
+        <p>42,00 €</p>
+      </div>
+      <div class="cart__item__content__settings">
+        <div class="cart__item__content__settings__quantity">
+          <p>Qté : </p>
+          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+        </div>
+        <div class="cart__item__content__settings__delete">
+          <p class="deleteItem">Supprimer</p>
+        </div>
+      </div>
+    </div> */
